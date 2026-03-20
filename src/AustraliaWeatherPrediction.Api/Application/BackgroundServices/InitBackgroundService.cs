d79e964f8af71033b97b28fb79cb5978d1d2f3c4
pavlo-kokhan <pavlokokhan@gmail.com>
2026-03-20 22:13:37 +0200

    model initialization log

--- src/AustraliaWeatherPrediction.Api/Application/BackgroundServices/InitBackgroundService.cs
+++ src/AustraliaWeatherPrediction.Api/Application/BackgroundServices/InitBackgroundService.cs
@@ -4,21 +4,28 @@ using System.Threading.Tasks;
 using AustraliaWeatherPrediction.Infrastructure.Onnx.Services.Abstract;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace AustraliaWeatherPrediction.Api.Application.BackgroundServices;
 
 public class InitBackgroundService : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<InitBackgroundService> _logger;
 
-    public InitBackgroundService(IServiceProvider serviceProvider)
+    public InitBackgroundService(IServiceProvider serviceProvider, ILogger<InitBackgroundService> logger)
     {
         _serviceProvider = serviceProvider;
+        _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Initializing WeatherPredictionService");
+        
         await InitializeWeatherPredictionServiceAsync();
+        
+        _logger.LogInformation("WeatherPredictionService is initialized");
     }
     
     private async Task InitializeWeatherPredictionServiceAsync()
